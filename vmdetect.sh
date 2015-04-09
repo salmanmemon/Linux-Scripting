290f10dae38d4f32a28ba4e002c12eb42ff6a3f3
Salman Memon <smemon92@gmail.com>
2015-04-09 09:51:56 +0530

    Update vmdetect.sh

--- vmdetect.sh
+++ vmdetect.sh
@@ -1,6 +1,6 @@
 # Script for detecting wheather you are in VM or not.
 #!/bin/bash
-  dmidecode | egrep -i 'VirtulBox|Vmware' > demo.txt # Detect the Virtual/Vmware information and store to demo.txt file
+  dmidecode | egrep -i 'VirtualBox|VMWare' > demo.txt # Detect the Virtual/Vmware information and store to demo.txt file
   if [ -s demo.txt ]; # Check the Contain of the demo.txt
   then
 
